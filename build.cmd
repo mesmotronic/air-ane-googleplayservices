1fabe15ea174ad3dbddd32baf3355a7c38e1d010
Neil Rackett <neil.rackett@mesmotronic.com>
2020-07-06 20:54:39 +0100

    Added support for Android-x64

--- build.cmd
+++ build.cmd
@@ -6,5 +6,6 @@ adt ^
  -swc swc/bin/air-ane-googleplayservices-swc.swc ^
  -platform Android-ARM -C android . ^
  -platform Android-ARM64 -C android . ^
+ -platform Android-x64 -C android . ^
  -platform Android-x86 -C android . ^
  -platform default -C default . 
